47cfdd5eacf05bb256a8573a2bd2d081d56a3b5c
Khushi <khushiawasthi8834@gmail.com>
2023-06-25 01:25:19 -0700

    Turtle

---
+++ T1.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+speed('slowest')
+
+fd(120)
+lt(45)
+bk(150)
+rt(90)
+
+mainloop()
---
+++ square.py
@@ -0,0 +1,15 @@
+from turtle import*
+
+speed('slowest')
+#square
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+fd(120)
+lt(90)
+
+hideturtle()
+mainloop()
